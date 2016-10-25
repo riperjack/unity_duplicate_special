110a173e7f371855fe11097a145f635ee895a01d
marcin <marcin.sewastianowicz@gmail.com>
2016-10-25 23:14:05 +0200

    Initial import

---
+++ DuplicateSpecial.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class DuplicateSpecial : EditorWindow
+{
+    int count;
+    Vector3 relOffset;
+    Vector3 conOffset;
+
+    // object to duplicate
+    GameObject target;
+    Renderer rend;
+
+    // wrapper object for duplicates
+    GameObject wrapper;
+
+    [MenuItem("Plugins/Duplicate special")]
+    static void Init()
+    {
+        DuplicateSpecial window = (DuplicateSpecial)GetWindow(typeof(DuplicateSpecial), true, "Duplicate special");
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        target = Selection.activeGameObject;
+        if (target == null)
+        {
+            ErrorMsg("Please select an object to duplicate from Hierarchy View");
+            return;
+        }
+
+        rend = target.GetComponentInChildren<Renderer>();
+
+        EditorGUI.BeginChangeCheck();
+
+        count = EditorGUILayout.IntField("Count", count);
+        relOffset = EditorGUILayout.Vector3Field("Relative offset", relOffset);
+        conOffset = EditorGUILayout.Vector3Field("Constant offset", conOffset);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Rollback(); // destroy wrapper from previous changes check tick before creating a new one
+            Apply();
+        }
+
+        if (GUILayout.Button("Ok")) Close();
+        if (GUILayout.Button("Cancel"))
+        {
+            Rollback();
+            Close();
+        }
+    }
+
+    private void Apply()
+    {
+        wrapper = new GameObject(target.name + "_duplicates_" + count);
+
+        for (int i = 1; i <= count; i++)
+        {
+            GameObject clone = Instantiate(target, target.transform.position, target.transform.rotation) as GameObject;
+
+            Vector3 offset = new Vector3(rend.bounds.size.x * relOffset.x + conOffset.x,
+                                        rend.bounds.size.y * relOffset.y + conOffset.y,
+                                        rend.bounds.size.z * relOffset.z + conOffset.z)
+                                        * i;
+            clone.transform.Translate(offset);
+            clone.transform.SetParent(wrapper.transform);
+        }
+    }
+
+    private void Rollback()
+    {
+        DestroyImmediate(wrapper);
+    }
+
+    private void ErrorMsg(string msg)
+    {
+        EditorGUILayout.LabelField(msg);
+    }
+
+    void OnDestroy()
+    {
+        if (count == 0) Rollback();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Duplicate special for Unity
+
+A tiny Unity Editor script which duplicates objects in a fashion similar to Blender's Array Modifier.
+
+Place it in your Unity project under Assets/Editor.
+
+![My image](https://github.com/marcin-se/unity_array/blob/master/duplicate_special_demo.gif) 	
+
